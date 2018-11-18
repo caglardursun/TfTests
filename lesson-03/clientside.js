6954f2db16fba8a1c122363c5b601d3d8aa29358
Çağlar Dursun <dursuncaglar@gmail.com>
2018-11-19 00:07:10 +0300

    Two input layer, 4 hidden layer, 3 output layer tried

--- lesson-03/clientside.js
+++ lesson-03/clientside.js
@@ -1,14 +1,13 @@
-var tf = require("@tensorflow/tfjs");
-
 // - 2 inputs 
 // - 4 hidden layer 
 // - 3 output (classification) 
 
 //This is the model
 const model = tf.sequential();
-//shape means dimensions 
-//Create input layer 
-//dense is a fully connected layer
+
+// Shape means dimensions first column then row
+// Create input layer 
+// Dense is a fully connected layer
 const hiddenLayer = tf.layers.dense({
     units:4, //numer of nodes
     inputShape:[2], //dimension of matrix input
@@ -38,29 +37,4 @@ model.compile({
 let inputs = tf.tensor1d([0.25,0.92],"float32");
 let outputs = model.predict(inputs);
 
-
-const xs = tf.tensor2d([
-    [0.25,0.12],
-    [0.5,0.9],
-    [0.35,0.82],
-    [0.15,0.2]
-]);
-
-
-const ys = tf.tensor2d([
-    [0.1,0.1,0.22],
-    [0.4,0.5,0.82],
-    [0.3,0.4,0.42]
-]);
-const history = model.fit(xs,ys);
-
-// const xs = tf.tensor2d([
-//     [0.25,0.12],
-//     [0.5,0.9],
-//     [0.35,0.82],
-//     [0.15,0.2]
-// ]);
-
-// let outputs = model.predict(xs);
-
-console.log(output);
+output.print();
